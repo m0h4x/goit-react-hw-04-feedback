c521db2b411a73406b6af6ee8c7157ebe78e79b6
m0h4x <86525723+m0h4x@users.noreply.github.com>
2022-03-18 15:49:31 +0100

    initial
    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,61 @@
-export const App = () => {
+import { useState } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Notification from './Notification';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const nameButton = ['Good', 'Neutral', 'Bad'];
+
+  const onLeaveFeedback = e => {
+    const name = e.currentTarget.innerText;
+    if (name === 'Good') {
+      setGood(good + 1);
+    }
+    if (name === 'Neutral') {
+      setNeutral(neutral + 1);
+    }
+    if (name === 'Bad') {
+      setBad(bad + 1);
+    }
+  };
+
+  const countTotalFeedback = () => {
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const positiveFeedback = Math.round((good / countTotalFeedback()) * 100);
+    return positiveFeedback;
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={nameButton}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 && (
+          <Notification message="There is no feedback" />
+        )}
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      </Section>
+    </>
   );
 };
+
+export default App;
